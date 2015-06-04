cb56e63662dfb806b909fc3b60054c5ff6934a60
Emmanuel AWA <emmanuel.awa@amadeus.com>
2015-06-04 17:52:57 -0400

    Using a new page that was generated by yo and adding a controller, changing the css via less.

--- app/src/modules/pages/my-page/MyPageCtrl.js
+++ app/src/modules/pages/my-page/MyPageCtrl.js
@@ -5,5 +5,22 @@
 
 angular.module('myApp').controller('MyPageCtrl', ['$scope',
 function($scope) {
+    $scope.items = [
+        {
+            title: 'title1'
+        },
+        {
+            title: 'title3'
+        },
+        {
+            title: 'title3'
+        },
+        {
+            title: 'title4'
+        },
+        {
+            title: 'title5'
+        },
+    ];
 	
 }]);

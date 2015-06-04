d595432dede11e6d9e3c49034297ffe21a712687
Emmanuel AWA <emmanuel.awa@amadeus.com>
2015-06-04 11:39:43 -0400

    Re-Scaffolding a directive after the generator had issues.

---
+++ app/src/modules/directives/my-directive/my-directive.js
@@ -0,0 +1,60 @@
+/**
+@toc
+
+@param {Object} scope (attrs that must be defined on the scope (i.e. in the controller) - they can't just be defined in the partial html). REMEMBER: use snake-case when setting these on the partial!
+TODO
+
+@param {Object} attrs REMEMBER: use snake-case when setting these on the partial! i.e. my-attr='1' NOT myAttr='1'
+    @param {String} customText just some ordinary text
+
+@dependencies
+TODO
+
+@usage
+partial / html:
+<div app-my-directive></div>
+TODO
+
+controller / js:
+TODO
+
+//end: usage
+*/
+
+'use strict';
+
+angular.module('app').directive('appMyDirective', [ function () {
+
+	return {
+		restrict: 'A',
+		scope: {
+		},
+
+		// replace: true,
+		template: function(element, attrs) {
+			var defaultsAttrs ={
+			};
+			for(var xx in defaultsAttrs) {
+				if(attrs[xx] ===undefined) {
+					attrs[xx] =defaultsAttrs[xx];
+				}
+			}
+
+            if (!attrs.customText) {
+                attrs.customText = "";
+            }
+			var html ="<div class='app-my-directive-cont'>"+
+				"my-directive"+
+				"<br />will last a life time"+
+                "<br />custom text: "+ attrs.customText +
+			"</div>";
+			return html;
+		},
+		
+		link: function(scope, element, attrs) {
+		},
+		
+		controller: function($scope, $element, $attrs) {
+		}
+	};
+}]);

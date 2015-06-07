99647e33f6332319472901e0026d80b994859d60
Emmanuel Awa <eawa@brandeis.edu>
2015-06-07 00:58:10 -0400

    Adding a new service. Cron was added to the package.json to specify it's version and then a new service was written (cron.js and cron.spec.js).
    Then the server.js was updated by copying a similar code for realtime service and modifying it to call the sample testRun function.

---
+++ app/modules/services/cron/cron.js
@@ -0,0 +1,28 @@
+/**
+Wraps angular-socket-io bower component factory (apparently this is the only way to connect on multiple addresses..)
+- https://github.com/btford/angular-socket-io
+
+@class socket
+
+@toc
+*/
+
+'use strict';
+
+var CronJob = require('cron').CronJob;
+var self;
+
+/**
+ @param {Object} opts
+ */
+function Cron(opts) {
+	self = this;
+}
+
+Cron.prototype.testRun = function(params) {
+    new CronJob('5 * * * * *', function(){
+        console.log('You will see this message every five second');
+    }, null, true, "America/New_York");
+};
+
+module.exports = new Cron({});
